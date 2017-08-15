dcc0db3b61222514b8ad6ad76733170662977248
Lucas Simon <lucassrod@gmail.com>
2017-08-14 22:21:21 -0300

    Finaliza o teste para atualizar o cliente

--- tests/test_rpc.py
+++ tests/test_rpc.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import sys
+
 import odoorpc
 
 import pytest
@@ -16,17 +18,12 @@ class OdooTestCaseBase(unittest.TestCase):
 
     def setUp(self):
 
-        self.customer_id = 0
-
         self.fake = Factory.create('pt_BR')
         # Prepare the connection to the server
         self.odoo = odoorpc.ODOO(
             'chocotech.trustcode.com.br',
             port=80
         )
-
-        # Check available databases
-        print(self.odoo.db.list())
 
         self.odoo.login(
             'chocotech',
@@ -50,7 +47,7 @@ class CustomerTestCase(OdooTestCaseBase):
 
         email = self.fake.email()
 
-        self.customer_id = Customer.create(
+        customer_id = Customer.create(
             {
                 'name': self.fake.name(),
                 'email': email,
@@ -59,9 +56,25 @@ class CustomerTestCase(OdooTestCaseBase):
             }
         )
 
-        customer = Customer.browse(self.customer_id)
+        customer = Customer.browse(customer_id)
 
         self.assertEqual(
             customer.email,
             email
+        )
+
+        # item 02 atualiza cliente
+
+        rg = self.fake.ssn()
+
+        customer.rg_fisica = rg
+
+        print 'Cliente criado com o emial: {} -> {}\n'.format(
+            customer.email,
+            customer.rg_fisica
+        )
+
+        self.assertEqual(
+            customer.rg_fisica,
+            rg
         )
